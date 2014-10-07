c801a6150e595a3d163fb8288866b507fc82e82a
Gabriel Carvahlo <enggabrielcarvalho@gmail.com>
2014-10-07 18:42:47 -0300

    Descrições em portugues

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,24 +1,16 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
- 
-  This example code is in the public domain.
- */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
+int led = 13; //Define a variavel led como 13
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+void setup() { //esta função é executada apenas uma vez
+
+  pinMode(led, OUTPUT);//Inicializa a porta digital 13 em modo saida
+  
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
-}
+void loop() {//esta função é executada infinitamente
+
+  digitalWrite(led, HIGH);//Liga o led com o parametro HIGH
+  delay(1000); //Espera um segundo
+  digitalWrite(led, LOW); //Desliga o led com o parametro LOW
+  delay(1000); //Espera um segundo
+  
+} 
